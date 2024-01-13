b227194c8ad2be5ab3e0f17190666c361c5bacdd
BolganD <kerosin.01@mail.ru>
2024-01-13 13:31:30 +0300

    add homework

--- dz/dz16.py
+++ dz/dz16.py
@@ -1,9 +1,9 @@
-def dec(args1, args2):
+def dec(args1, args2, o):
     def arg_dec(fn):
         def wrap(v, x, y, z):
             len_nums = len(fn(v, x, y, z))
             # summa_sah = (v + x + y + z) / len_nums
-            print(args1, v, ', ', x, ', ', y, ', ', z, args2, (v + x + y + z) / len_nums, sep='')
+            print(args1, v, o, x, o, y, o, z, args2, (v + x + y + z) / len_nums, sep='')
         return wrap
     return arg_dec
 
@@ -13,7 +13,7 @@ def summa(a, b, c, d):
     print('Сумма чисел ', a, ', ', b, ', ', c, ', ', d, ' = ', s, sep='')
 
 
-@dec('Среднее арифметическое чисел ', ' = ')
+@dec('Среднее арифметическое чисел ', ' = ', ', ')
 def sub(a, b, c, d):
     return a, b, c, d
 
