56c77e26aeebf89c2f6ec3797da4095548c271e8
BolganD <kerosin.01@mail.ru>
2024-05-07 11:50:32 +0300

    add

--- main.py
+++ main.py
@@ -6789,12 +6789,39 @@ import sqlite3
 # cur.execute('''''')
 # con.close()
 
-with sqlite3.connect('profile.db') as con:
+# with sqlite3.connect('profile.db') as con:
+#     cur = con.cursor()
+#     # cur.execute('''CREATE TABLE IF NOT EXISTS users(
+#     # id INTEGER PRIMARY KEY AUTOINCREMENT,
+#     # name TEXT NOT NULL,
+#     # summa REAL,
+#     # date TEXT
+#     # )''')
+#     cur.execute('DROP TABLE users')
+
+
+with sqlite3.connect('users.db') as con:
     cur = con.cursor()
-    # cur.execute('''CREATE TABLE IF NOT EXISTS users(
+    # cur.execute('''
+    # CREATE TABLE IF NOT EXISTS person(
     # id INTEGER PRIMARY KEY AUTOINCREMENT,
     # name TEXT NOT NULL,
-    # summa REAL,
-    # date TEXT
+    # phone BLOB NOT NULL DEFAULT "+79510000000",
+    # age INTEGER CHECK(age > 0 AND age < 100),
+    # email TEXT UNIQUE
     # )''')
-    cur.execute('DROP TABLE users')
+    # cur.execute('''
+    # ALTER TABLE person
+    # RENAME TO person_table;
+    # ''')
+    # cur.execute('''
+    # ALTER TABLE person_table
+    # ADD COLUMN address1 TEXT NOT NULL DEFAULT 'г. Сочи';
+    # ''')
+    # cur.execute('''
+    # ALTER TABLE person_table
+    # RENAME COLUMN address TO home_address;
+    # ''')
+    cur.execute('''
+    DROP TABLE person_table
+    ''')
